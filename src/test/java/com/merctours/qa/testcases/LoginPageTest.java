914fab8427d92418abff4f82b95c88f7d9fd24a9
Bharathy <bharathy78@gmail.com>
2019-05-16 13:23:07 -0500

    Log4j cases added

--- src/test/java/com/merctours/qa/testcases/LoginPageTest.java
+++ src/test/java/com/merctours/qa/testcases/LoginPageTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import org.apache.log4j.Logger;
 import com.merctours.qa.base.TestBase;
 import com.merctours.qa.pages.FlightSearchPage;
 import com.merctours.qa.pages.LoginPage;
@@ -12,6 +13,7 @@ import com.merctours.qa.pages.LoginPage;
 public class LoginPageTest extends TestBase{
 	LoginPage signinpage;
 	FlightSearchPage flsearch;
+	Logger log = Logger.getLogger(LoginPageTest.class);
 	
 	public LoginPageTest(){
 		super();		
@@ -25,18 +27,27 @@ public class LoginPageTest extends TestBase{
 	
 	@Test(priority=1)
 	public void loginPageTitleTest(){
+		log.info("****************************** starting test case *****************************************");
+		log.info("****************************** Mercury Tours Login Page title Test *****************************************");
 		String title = signinpage.validateLoginPageTitle();
 		Assert.assertEquals(title, "Sign-on: Mercury Tours");
+		log.info("****************************** ending test case *****************************************");
+		log.info("****************************** Mercury Tours Login Page title Test *****************************************");
 	}
 	
 	@Test(priority=2)
 	public void loginTest() {
+		log.info("****************************** starting test case *****************************************");
+		log.info("****************************** Mercury Tours Login Test *****************************************");
 		flsearch = signinpage.signin(prop.getProperty("username"), prop.getProperty("password"));
+		log.info("****************************** ending test case *****************************************");
+		log.info("****************************** Mercury Tours Login Test *****************************************");
 	}
 	
 	@AfterMethod
 	public void tearDown() {
-		//driver.quit();		
+		driver.quit();
+		log.info("****************************** Browser is closed *****************************************");
 	}
 	
 }
